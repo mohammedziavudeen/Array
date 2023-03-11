863c4d27772d494193f5ab82a353b04d6346ee24
mohammedziavudeen <mdziavu443@gmail.com>
2023-03-11 08:56:41 +0530

    Array

---
+++ Subset.java
@@ -0,0 +1,28 @@
+package array;
+
+public class Subset {
+
+	public static void main(String[] args) {
+		int[] n= {3,4,6,18,28,41};
+		int[] n1= {28,6,4,0};
+		boolean appeared=false;
+		for(int j=0;j<n1.length;j++) {
+			boolean match=false;
+			for(int i=0;i<n.length;i++) {
+				if(n1[j]==n[i]) {
+					match=true;
+					break;
+				}		
+			}
+			if(match==false) {
+				appeared=true;
+				System.out.println("It is  not subset");
+				break;
+			}
+		}
+		if(appeared==false) {
+			System.out.println("It is subset of array");
+		}
+		
+	}
+}
